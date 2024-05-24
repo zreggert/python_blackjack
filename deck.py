e329419e5e23510575f2912ef0aabf9ace065a57
Zack Eggert <zreggert@gmail.com>
2024-05-24 15:56:21 -0400

    added functionality to allow for splitting a hand if the value of the cards in the starting hand are the same. had to rewrite a good bit of the other functions to check for these changes when a player has more than one hand.

--- deck.py
+++ deck.py
@@ -12,6 +12,7 @@ def new_deck():
 
 def check_deck(deck_id):
     response = requests.get(f'https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=0')
+    print(response.status_code)
     remaining = int(response.json()['remaining'])
     if remaining > 100:
         return True
@@ -28,6 +29,8 @@ def get_hand(deck_id):
         card = (item['value'], item['suit'])
         hand.append(card)
 
+    # test_hand_data = [('2', 'SPADES'), ('2', "HEARTS")]
+    # return test_hand_data
     return hand
 
 def draw_a_card(deck_id, hand):
@@ -39,39 +42,56 @@ def draw_a_card(deck_id, hand):
     
     return hand
 
+# function for splitting a list of cards into 2 lists of cards
+def split_hand(hand, deck):
+    print(hand)
+    first_card = hand[:1]
+    second_card = hand[1:]
+
+    hand1 = draw_a_card(deck, first_card)
+    hand2 = draw_a_card(deck, second_card)
+
+    hands = [hand1,  hand2]
+    print(hands)
+
+    return hands
+
+    
+    
+
+
 
 # play hand function will call get_hand function to create plaer and dealer hands
-def play_hand(bet, deck):
-    hand_data = {}
+def deal_hands(deck):
+    hands= {}
 
     player_hand = get_hand(deck)
-    player_sum = sum_of_hand(player_hand)
-
+    hands["player_hands"] = [player_hand]
     dealer_hand = get_hand(deck)
-    dealer_sum = sum_of_hand(dealer_hand)
-    showing = dealer_hand[0]
+    hands["dealer_hand"] = dealer_hand
+    hands["showing"] = hands["dealer_hand"][0]
+    
+    return hands
 
-    # putting data into a dictionary that will be returned at the end of the function. this data will be used to determine the class player method that will be envoked later
+
+def get_game_data(hands, deck, bet):
+    hand_data = {}
+    hand_id = 1
     hand_data["deck_id"] = deck
+    hand_data["dealer_hand"] = hands["dealer_hand"]
+    hand_data["dealer_sum"] = sum_of_hand(hands["dealer_hand"])
+    hand_data["player_hands"] = []
+    for hand in hands["player_hands"]:
+        new_hand = {}
+        new_hand["hand_id"] = hand_id
+        new_hand["hand"] = hand
+        new_hand["player_sum"] = sum_of_hand(hand)
+        new_hand["bet"] = bet
+        new_hand["move"] = ""
+        hand_data["player_hands"].append(new_hand)
+        hand_id += 1
 
-    hand_data["bet"] = bet
-    hand_data["player_hand"] = player_hand
-    hand_data["player_sum"] = player_sum
-    hand_data["dealer_hand"] = dealer_hand
-    hand_data["dealer_sum"] = dealer_sum
-    hand_data["move"] = ""
-    
-    print(player_hand)
-    print(f"You have a {player_sum}.")
-    # need to add functionality if player has blackjack
-
-    print(f"Dealer is showing {showing}.")
-
-    if showing[0] == "ACE" or showing[0] == "KING" or showing[0] == "QUEEN" or showing[0] == "JACK":
-        check_for_blackjack(dealer_sum)
-        # need to add fucntionality for if dealer has blackjack
     return hand_data
-    
 
 # checks is a hand is a blackjack
 def check_for_blackjack(num):
@@ -96,19 +116,27 @@ def sum_of_hand(hand):
     
     return total
 
-def compare_hands(data):
-    if data["player_sum"] > data["dealer_sum"]:
+def compare_hands(hand, data):
+    if hand["player_sum"] > data["dealer_sum"]:
         return "win"
-    elif data["dealer_sum"] > data["player_sum"]:
+    elif data["dealer_sum"] > hand["player_sum"]:
         return "lost"
-    elif data["player_sum"] == data["dealer_sum"]:
+    elif hand["player_sum"] == data["dealer_sum"]:
         return "draw"
 
+def check_for_pair(hand):
+    if hand[0][0] == hand[1][0]:
+        return True
+
+
+# test_hand = [('2', 'SPADES'), ('2', "HEARTS")]
+test_hand_data = {'player_hand': [('2', 'SPADES'), ('2', "HEARTS")]}
 
 
 
+# check_for_pair(test_hand)
 
 
-
-
-# new_deck()
+# new_deck()Portfolio Project: Terminal Game - Blackjack
+# if check_for_pair(test_hand_data["player_hand"]):
+#         split = input(f"Would you like to split your {test_hand_data["player_hand"][0][0]}s?")
--- script.py
+++ script.py
@@ -11,31 +11,36 @@ class Player:
     def place_bet(self):
         print("How much would you like to bet?")
         while True:
-            self.bet = int(input())
-            if self.bet < 5:
-                print("The table minimum per hand is $5.")
-            elif self.bet > self.bankroll:
-                print("Sorry, you do not have enough to money for that bet.")
-            else:
-                return self.bet
+            try:
+                self.bet = int(input())
+                if self.bet < 5:
+                    print("The table minimum per hand is $5.\n")
+                elif self.bet > self.bankroll:
+                    print("Sorry, you do not have enough to money for that bet.\n")
+                else:
+                    return self.bet
+            except ValueError:
+                print("Please enter a numeric value of at least $5 to play your next hand.\n")
+
     
-    def player_hit(self, data):
-        hand = data["player_hand"]
-        deck_id = data["deck_id"]
-        new_hand = deck.draw_a_card(deck_id, hand)
-        data["player_hand"] = new_hand
-        print(data["player_hand"])
-        data["player_sum"] = deck.sum_of_hand(data["player_hand"])
-        if data["player_sum"] > 21:
-            data["move"] = "end hand"
-            print(f"Sorry you bust with a {data["player_sum"]}.")
+    def player_hit(self, hand , deck_id):
+        print(hand)
+        new_hand = deck.draw_a_card(deck_id, hand["hand"])
+        hand["hand"] = new_hand
+        print(hand["hand"])
+        hand["player_sum"] = deck.sum_of_hand(hand["hand"])
+        if hand["player_sum"] > 21:
+            hand["move"] = "end hand"
+            print(f"Sorry you bust with a {hand["player_sum"]}.")
         else:
-            print(f"You now have {data["player_sum"]}")
+            print(f"You now have {hand["player_sum"]}")
 
     def hit_or_stay(self, data):
+        data["move"] = ""
         move = input("Hit? or Stay?\n").lower()
         if move != "hit" and move != "stay":
-            move = input("Hit? or Stay?\n")
+            print("Please enter either hit or stay.")
+            # move = input("Hit? or Stay?\n").lower()
         elif move == "hit":
                 # call hit function
             print("player hit")
@@ -49,13 +54,28 @@ class Player:
             return data
         else:
             print("something else")
+
+    def split(self, hand):
+        # if deck.check_for_pair(hand):
+        while True:
+            split = input(f"Would you like to split your {hand[0][0]}s? Yes or No?\n").lower()
+            try:
+                if split == "no":
+                    return False
+                elif split == "yes":
+                        # deck.split_hand(hand)
+                    return True
+                else:
+                    print("Please enter either yes or no.")
+            except: 
+                pass
     
-    def player_wins(self, data):
-        self.bankroll += data["bet"]
+    def player_wins(self, hand):
+        self.bankroll += hand["bet"]
         print(f"You win! you now have ${self.bankroll}.")
 
-    def player_loses(self, data):
-        self.bankroll -= data["bet"]
+    def player_loses(self, hand):
+        self.bankroll -= hand["bet"]
         print(f"Sorry, the dealer wins. You now have ${self.bankroll}")
         
 
@@ -81,10 +101,8 @@ while True:
             print("You must start with at least $100.\n")
             continue
     except ValueError:
-        print("Please be sure to enter numeric value of the money you want to play with.")
+        print("Please be sure to enter numeric value of the money you want to play with.\n")
         
-
-player1 = Player(name, bankroll)
 
 def start(player):
     deck_id = deck.new_deck()
@@ -93,28 +111,53 @@ def start(player):
 
 def deal(player, deck_data):
     bet = player.place_bet()
-    hand_data = deck.play_hand(bet, deck_data)
+    hands = deck.deal_hands(deck_data)
+    print(f"You have {hands["player_hands"]}.")
+    print(f"Dealer is showing {hands["showing"]}")
 
-    if deck.check_for_blackjack(hand_data["player_sum"]) == True and deck.check_for_blackjack(hand_data["dealer_sum"]) == False:
-        print("BLACKJACK! Dealer is checking their hand.")
-        player.player_wins(hand_data)
-    elif deck.check_for_blackjack(hand_data["dealer_sum"]) == True:
-        print("Dealer has Blackjack.")
-        player.player_loses(hand_data)
-    elif deck.check_for_blackjack(hand_data["player_sum"]) == True and deck.check_for_blackjack(hand_data["dealer_sum"]) == True:
-        print("Blackjack! Unfortunately dealer all so has Blackjack. Player pushes.")
-    else:
-        while hand_data["move"] != "end hand":
-            player.hit_or_stay(hand_data)
-            if hand_data['move'] == "hit":
-                player.player_hit(hand_data)
-                if hand_data["player_sum"] > 21:
-                    player.player_loses(hand_data)
-            elif hand_data['move'] == "stay":
-                dealers_turn(player, hand_data)
+    for hand in hands["player_hands"]:
+        if deck.check_for_pair(hand):
+            if player.split(hand):
+                hands["player_hands"] = deck.split_hand(hand, deck_data)
+    data = deck.get_game_data(hands, deck_data, bet)
 
-    play_again(player, deck_data)
+    any_blackjacks(player, data)
+    play_hand(player, data)
 
+
+def any_blackjacks(player, data):
+    for hand in data["player_hands"]:
+        if deck.check_for_blackjack(hand["player_sum"]) == True and deck.check_for_blackjack(data["dealer_sum"]) == False:
+            print("BLACKJACK! Dealer is checking their hand.")
+            player.player_wins(hand)
+        elif deck.check_for_blackjack(data["dealer_sum"]) == True:
+            print("Dealer has Blackjack.")
+            player.player_loses(hand)
+        elif deck.check_for_blackjack(hand["player_sum"]) == True and deck.check_for_blackjack(data["dealer_sum"]) == True:
+            print("Blackjack! Unfortunately dealer all so has Blackjack. Player pushes.")
+        else:
+            pass
+
+
+
+def play_hand(player, data):
+    print(data)
+    deck_id = data["deck_id"]
+    for hand in data["player_hands"]:
+        print(f"Playing hand {hand["hand"]}.")
+        print(f"Sum of the hand is {hand["player_sum"]}.")
+        while hand["move"] != "end hand":
+            player.hit_or_stay(hand)
+            if hand['move'] == "hit":
+                player.player_hit(hand, deck_id)
+                if hand["player_sum"] > 21:
+                    player.player_loses(hand)
+            elif hand['move'] == "stay":
+                break
+    print(data)
+    dealers_turn(player, data)
+
+    play_again(player, deck_id)
         
 def dealers_turn(player, data):
     print("Dealers turn.")
@@ -127,41 +170,37 @@ def dealers_turn(player, data):
         print(new_hand)
         data["dealer_sum"] = deck.sum_of_hand(new_hand)
     
-    if data["dealer_sum"] >= 17 and data["dealer_sum"] <= 21:
-        print(f"Dealer has a {data["dealer_sum"]}")
-        result = deck.compare_hands(data)
-        if result == "win":
-            player.player_wins(data)
-        elif result == "lost":
-            player.player_loses(data)
-        elif result == 'draw':
-            print(f"Both you and the dealer have a {data["dealer_sum"]}. Player pushes.")
-    elif data["dealer_sum"] > 21:
-        print(f"Dealer busts with a {data["dealer_sum"]}")
-        player.player_wins(data)
+    end_hand(player, data)
+    
+def end_hand(player, data):
+    for hand in data["player_hands"]:
+        if hand["move"] == "stay":
+            if data["dealer_sum"] >= 17 and data["dealer_sum"] <= 21:
+                print(f"Dealer has a {data["dealer_sum"]}")
+                result = deck.compare_hands(hand, data)
+                if result == "win":
+                    player.player_wins(hand)
+                elif result == "lost":
+                    player.player_loses(hand)
+                elif result == 'draw':
+                    print(f"Both you and the dealer have a {data["dealer_sum"]}. Player pushes.")
+            elif data["dealer_sum"] > 21:
+                print(f"Dealer busts with a {data["dealer_sum"]}")
+                player.player_wins(hand)
 
     data["move"] = "end hand"
 
-    # play_again(player, data)
 
+def play_again(player, deck_id):
 
-def play_again(player, deck_data):
-    if deck.check_deck(deck_data):
-        deal(player, deck_data)
+    if deck.check_deck(deck_id):
+        deal(player, deck_id)
     else:
         start(player)
 
 
-
-
-
-
-
+player1 = Player(name, bankroll)
 start(player1)
-
-
-
-
 
 
 
